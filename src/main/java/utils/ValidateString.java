03ecdbb632197d2ca2ead40f16d08e5ba89a68c5
BuildTools <unconfigured@null.spigotmc.org>
2021-06-10 18:58:12 -0300

    ValidateString.java en el utils

---
+++ src/main/java/utils/ValidateString.java
@@ -0,0 +1,22 @@
+package main.java.utils;
+
+import main.java.exception.ValidationException;
+
+public class ValidateString {
+
+
+    ValidationException error;
+
+    public String validate(String string){
+
+        if(string == null){
+
+            System.out.println();
+
+        }else {
+
+            return string.trim();
+        }
+    }
+
+}
